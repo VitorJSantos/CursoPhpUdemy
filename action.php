39da61a3c7510553d68393afaa557283c56423e7
Vitor Santos <vitor.jsantos@outlook.com>
2021-07-17 21:38:07 -0300

    adicionando todos os arquivos da pasta

---
+++ action.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'Olá ' . htmlspecialchars($_POST["nome"]) . ', você tem ' . ($_POST["idade"]. ' anos.');
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<title>PHP</title>
+
+<form action ="action.php" method="post">
+Qual o seu Nome: <p><input type="text" name="nome"><br></p>
+Qual a sua Idade:<p><input type="text" name="idade"><br></p>
+<input type ="submit" value="Enviar">
+
+</form>
+</body>
+</html>
